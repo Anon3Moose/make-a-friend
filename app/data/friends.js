fbc43af5becf246407fd70885152ec3e053c1016
Vestige <jaracomail@gmail.com>
2019-10-15 23:52:37 -0700

    update

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: 'Harold',
+		photo: 'https://www.dailydot.com/wp-content/uploads/6d6/a0/740bf5f811111c88-2048x1024.jpg',
+		scores: [5, 1, 2, 3, 1, 2, 3, 4, 2, 4]
+	},
+	{
+		name: 'Amy',
+		photo: 'https://pbs.twimg.com/media/DFpXnENW0AEYPuH.jpg',
+		scores: [2, 5, 4, 1, 2, 3, 5, 1, 3, 2]
+	},
+	{
+		name: 'Karen',
+		photo: 'https://i.redd.it/1kysiwq6pev21.jpg',
+		scores: [1, 1, 5, 3, 3, 5, 4, 2, 1, 3]
+	},
+	{
+		name: 'https://i.pinimg.com/originals/a5/41/02/a541024b0989ea9b0bd470fcfc89dcb1.jpg',
+		photo: '',
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 4]
+	},
+	{
+		name: 'Elon-chan',
+		photo: 'https://cdn.vox-cdn.com/thumbor/F1amWkSpCu_1dFRdKkIX2oXDIB8=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/14253063/Screen_Shot_2019_02_22_at_3.13.37_PM.png',
+		scores: [3, 1, 4, 4, 2, 4, 5, 1, 5, 5]
+	}
+];
+
+module.exports = friends;
