901237a08029166197c90e5801a9521673bc04ae
Vestige <jaracomail@gmail.com>
2020-01-07 17:43:31 -0800

    patch

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 var app = express();
 
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/app/css'));
 app.use(bodyParser.urlencoded({ extended: true }));
